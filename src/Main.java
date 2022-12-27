91b3913e8fc21ffdddf163e2afffb669ebde9781
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-12-27 19:32:09 +0100

    JPanel = a GUI component that functions as a container to hold other components

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,46 @@
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        ImageIcon image = new ImageIcon("src/bro.png");
+        ImageIcon icon = new ImageIcon("src/up.png");
 
-        Border border = BorderFactory.createLineBorder(Color.green,3);
+        JLabel label =new JLabel();
+        label.setText("Hi");
+        label.setIcon(icon);
+      //  label.setVerticalAlignment(JLabel.BOTTOM);
+      //  label.setHorizontalAlignment(JLabel.RIGHT);
+        label.setBounds(0,0,175,175);
 
-        JLabel label = new JLabel();
-        label.setText("Hello World");
-        label.setIcon(image);
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.TOP);
-        label.setForeground(Color.GREEN);
-        label.setFont(new Font( "MV Boli", Font.PLAIN,30));
-        label.setIconTextGap(30);
-        label.setBackground(Color.blue);
-        label.setOpaque(true);
-        label.setBorder(border);
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setHorizontalAlignment(JLabel.CENTER);
-//        label.setBounds(100,100,300,300);
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0,0,250,250);
+        redPanel.setLayout(null);
 
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(250,0,250,250);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0,250,500,250);
+        greenPanel.setLayout(null);
+//        greenPanel.setLayout(new BorderLayout());
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.setSize(500,500);
-//        frame.setLayout(null);
+        frame.setLayout(null);
+        frame.setSize(750,750);
         frame.setVisible(true);
-        frame.add(label);
-        frame.pack();
+
+        redPanel.add(label);
+
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);
+
+
+
     }
 }
