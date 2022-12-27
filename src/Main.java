bf00dc1e35ab0e8c5372ef68b462e5a0a39bf6a4
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-12-27 19:13:13 +0100

    Create label

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        new MyFrame();
+
+        ImageIcon image = new ImageIcon("src/bro.png");
+
+        Border border = BorderFactory.createLineBorder(Color.green,3);
+
+        JLabel label = new JLabel();
+        label.setText("Hello World");
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(Color.GREEN);
+        label.setFont(new Font( "MV Boli", Font.PLAIN,30));
+        label.setIconTextGap(30);
+        label.setBackground(Color.blue);
+        label.setOpaque(true);
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+//        label.setBounds(100,100,300,300);
+
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(500,500);
+//        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.add(label);
+        frame.pack();
     }
 }
