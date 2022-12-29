d6f35b0eaaa7cce16d9ff6cf18113bf40fa6eba2
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-12-29 18:22:47 +0100

    JTextField a GUI textbox component that can be used to add, set, or get text

---
+++ src/TextBoxComponent/TextboxComponent.java
@@ -0,0 +1,12 @@
+package TextBoxComponent;
+
+import javax.swing.*;
+
+public class TextboxComponent {
+    public static void main(String[] args) {
+
+//        JTextField = A GUI textbox component that can be used to add, set, or get text
+
+        MyFrame frame = new MyFrame();
+    }
+}
